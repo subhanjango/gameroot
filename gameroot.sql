31b9005c02f91d5e3ab01687179f677330455507
subhanjango <subhannizarcha@gmail.com>
2017-09-29 04:08:12 +0500

    New DB

--- gameroot.sql
+++ gameroot.sql
@@ -40,8 +40,64 @@ CREATE TABLE `admins` (
 
 LOCK TABLES `admins` WRITE;
 /*!40000 ALTER TABLE `admins` DISABLE KEYS */;
-INSERT INTO `admins` VALUES (1,'subhan.nizar@yahoo.com','21232f297a57a5a743894a0e4a801fc3',0,NULL,NULL,NULL),(7,'subhan@asd.com','53b76cc34444f5b08f9a0a333437e32d',1,'2017-09-23 12:34:11','2017-09-23 16:17:51',NULL),(8,'nadir@asd.com','53b76cc34444f5b08f9a0a333437e32d',0,'2017-09-23 12:34:41','2017-09-23 16:31:45',NULL),(9,'asdasd@adsd.com','53b76cc34444f5b08f9a0a333437e32d',1,'2017-09-23 12:56:58','2017-09-23 16:31:38',NULL);
+INSERT INTO `admins` VALUES (1,'subhan.nizar@yahoo.com','53b76cc34444f5b08f9a0a333437e32d',0,'2017-09-23 12:34:11','2017-09-23 16:17:51',NULL),(7,'subhan@asd.com','53b76cc34444f5b08f9a0a333437e32d',1,'2017-09-23 12:34:11','2017-09-23 16:17:51',NULL),(8,'nadir@asd.com','53b76cc34444f5b08f9a0a333437e32d',0,'2017-09-23 12:34:41','2017-09-23 16:31:45',NULL),(9,'asdasd@adsd.com','53b76cc34444f5b08f9a0a333437e32d',1,'2017-09-23 12:56:58','2017-09-23 16:31:38',NULL);
 /*!40000 ALTER TABLE `admins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `created_at` varchar(45) DEFAULT NULL,
+  `updated_at` varchar(45) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (2,'As',0,'2017-09-25 22:12:23','2017-09-25 22:22:17',1),(3,'Test243',1,'2017-09-25 22:16:53','2017-09-25 22:20:03',1);
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `groups`
+--
+
+DROP TABLE IF EXISTS `groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_title` varchar(45) DEFAULT NULL,
+  `status` smallint(3) DEFAULT NULL,
+  `created_by` tinyint(3) DEFAULT NULL,
+  `created_at` varchar(45) DEFAULT NULL,
+  `updated_at` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `groups`
+--
+
+LOCK TABLES `groups` WRITE;
+/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
+INSERT INTO `groups` VALUES (1,'Asdasdasd123',1,1,'2017-09-28 21:02:52','2017-09-28 22:21:37');
+/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -78,13 +134,15 @@ DROP TABLE IF EXISTS `questions`;
 CREATE TABLE `questions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `question_title` varchar(45) DEFAULT NULL,
-  `question_description` varchar(45) DEFAULT NULL,
+  `question_description` longtext,
+  `subcategory_id` int(11) DEFAULT NULL,
   `status` smallint(5) DEFAULT NULL,
+  `solutions` longtext,
   `created_by` varchar(45) DEFAULT NULL,
   `created_at` varchar(45) DEFAULT NULL,
   `updated_at` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,7 +151,88 @@ CREATE TABLE `questions` (
 
 LOCK TABLES `questions` WRITE;
 /*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+INSERT INTO `questions` VALUES (1,'Asdasd','&lt;p&gt;asdasdasd&lt;/p&gt;',1,1,NULL,'1','2017-09-28 18:56:12','2017-09-28 18:56:12');
 /*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `solutions`
+--
+
+DROP TABLE IF EXISTS `solutions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `solutions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question_id` int(11) DEFAULT NULL,
+  `yes` longtext,
+  `no` longtext,
+  `created_at` varchar(45) DEFAULT NULL,
+  `updated_at` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `solutions`
+--
+
+LOCK TABLES `solutions` WRITE;
+/*!40000 ALTER TABLE `solutions` DISABLE KEYS */;
+INSERT INTO `solutions` VALUES (1,1,'&lt;p&gt;dasdasd&lt;/p&gt;','&lt;p&gt;dasdasdas&lt;/p&gt;',NULL,NULL),(2,1,'&lt;p&gt;asdasdassd&lt;/p&gt;','&lt;p&gt;dsadasd&lt;/p&gt;',NULL,NULL),(3,1,'&lt;p&gt;asdasdas&lt;/p&gt;','&lt;p&gt;asdasdasdsdasdasdasd&lt;/p&gt;',NULL,NULL);
+/*!40000 ALTER TABLE `solutions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sub_categories`
+--
+
+DROP TABLE IF EXISTS `sub_categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sub_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `created_at` varchar(45) DEFAULT NULL,
+  `updated_at` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sub_categories`
+--
+
+LOCK TABLES `sub_categories` WRITE;
+/*!40000 ALTER TABLE `sub_categories` DISABLE KEYS */;
+INSERT INTO `sub_categories` VALUES (1,'Saasdasdasd',1,3,1,'2017-09-26 20:04:57','2017-09-26 20:04:57'),(3,'Asdasdasdasdd',1,3,1,'2017-09-26 20:45:18','2017-09-26 20:45:18'),(4,'Dsaassa',1,3,1,'2017-09-26 22:01:37','2017-09-26 22:01:37');
+/*!40000 ALTER TABLE `sub_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_groups`
+--
+
+DROP TABLE IF EXISTS `user_groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_groups` (
+  `group_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_groups`
+--
+
+LOCK TABLES `user_groups` WRITE;
+/*!40000 ALTER TABLE `user_groups` DISABLE KEYS */;
+INSERT INTO `user_groups` VALUES (1,4),(1,5);
+/*!40000 ALTER TABLE `user_groups` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -104,15 +243,15 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` mediumtext,
+  `password` mediumtext,
+  `status` int(11) DEFAULT NULL,
+  `created_at` varchar(45) DEFAULT NULL,
+  `updated_at` varchar(45) DEFAULT NULL,
+  `created_by` smallint(3) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -121,7 +260,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Admin','subhannizarcha@gmail.com','$2y$10$vXMvavPgTcoDgy/OXCUATOU9vdjMOSXF51rZa7XOYqleTKqxdVNv6','4P1tmIBLAts7gC2JaqhhgE7ePH5K2kzoz3nXPLyPVQhsFXuFpCbw9CH0vIO4','2017-09-21 13:26:40','2017-09-21 13:26:40');
+INSERT INTO `users` VALUES (4,'subhan.nizar@yahoo.com','53b76cc34444f5b08f9a0a333437e32d',1,'2017-09-28 20:25:24','2017-09-28 20:25:24',1),(5,'rehmanisaghani95@gmail.com','53b76cc34444f5b08f9a0a333437e32d',1,'2017-09-28 20:25:31','2017-09-28 20:25:31',1),(7,'asdadasd@adsda.com','53b76cc34444f5b08f9a0a333437e32d',1,'2017-09-28 22:43:02','2017-09-28 22:43:02',1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -134,4 +273,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-09-23 22:19:37
+-- Dump completed on 2017-09-29  4:07:16
